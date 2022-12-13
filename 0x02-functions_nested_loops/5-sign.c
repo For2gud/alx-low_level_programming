bbdd1a7c926df09cc6373616d5622d0f169a096e
Fortune Joseph <fortunejoseph917@gmail.com>
2022-12-13 15:43:36 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{ 
+	{
 		_putchar('+');
 		return (1);
 	}
@@ -21,8 +21,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar('_');
-			return (-1);
+		_putchar('-')
+		return (-1);
 	}
 }
-		
