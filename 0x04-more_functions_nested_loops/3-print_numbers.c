b4cee11d648c20cfcbe3a90006453dbace8912e0
Fortune Joseph <fortunejoseph917@gmail.com>
2022-12-15 09:01:50 -0800

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * print_numbers - function that print all numbers from 0-9
+ * Return: Always 0
  */
 
 void print_numbers(void)
 {
-	int n;
+	int a = 0;
 
-	for (n = 0; n <= 9; n++)
-	{	
-	_putchar((n % 10) + '0');
-	}
+	while (a < 10)
+		_putchar(a++ + '0');
+
 	_putchar('\n');
 }
