421f786a83ca95cd024c2f9f8e81f5b7d2441159
Fortune Joseph <fortunejoseph917@gmail.com>
2022-12-20 01:09:11 -0800

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line to stdout
- * @str: pointer to string
+ * _puts - prints a string
+ * @str: string to be printed
  */
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*str++);
 	}
-
 	_putchar('\n');
 }
