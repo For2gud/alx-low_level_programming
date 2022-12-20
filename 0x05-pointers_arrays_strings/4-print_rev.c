f3f1d98cc03fbcedf648111ce464c787cc4af578
Fortune Joseph <fortunejoseph917@gmail.com>
2022-12-20 01:49:26 -0800

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * print_rev - print a string in reverse
- * @s: char array string type
+ * print_rev -> printing a string in reverse
+ * @s: the string to be printed in rev
  */
 
 void print_rev(char *s)
 {
-	int i;
+	int i, n;
 
-	for (i = 0; s[i] != '\0'; i++)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
 
-
-	for (i--; i >= 0; i--)
+	for (i = n - 1; i >= 0; i--)
+	{
 		_putchar(s[i]);
-
+	}
 	_putchar('\n');
 }
